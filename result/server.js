bfcfba5de2747b1ce626205253e12ecd1d273312
Ramon Lamana <rlamana@gmail.com>
2023-11-08 17:43:45 +0100

    Fix quotes

--- result/server.js
+++ result/server.js
@@ -1,27 +1,27 @@
-var express = require("express"),
+var express = require('express'),
   async = require('async'),
-  pg = require("pg"),
-  path = require("path"),
-  cookieParser = require("cookie-parser"),
-  methodOverride = require("method-override"),
+  pg = require('pg'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  methodOverride = require('method-override'),
   app = express(),
-  server = require("http").Server(app),
-  io = require("socket.io")(server);
+  server = require('http').Server(app),
+  io = require('socket.io')(server);
 
-io.set("transports", ["polling"]);
+io.set('transports', ['polling']);
 
 var port = process.env.PORT || 4000;
 
-io.sockets.on("connection", function (socket) {
-  socket.emit("message", { text: "Welcome!" });
+io.sockets.on('connection', function (socket) {
+  socket.emit('message', { text: 'Welcome!' });
 
-  socket.on("subscribe", function (data) {
+  socket.on('subscribe', function (data) {
     socket.join(data.channel);
   });
 });
 
 var pool = new pg.Pool({
-  connectionString: "postgres://okteto:okteto@postgresql/votes",
+  connectionString: 'postgres://okteto:okteto@postgresql/votes',
 });
 
 async.retry(
@@ -29,31 +29,31 @@ async.retry(
   function (callback) {
     pool.connect(function (err, client, done) {
       if (err) {
-        console.error("Waiting for db", err);
+        console.error('Waiting for db', err);
       }
       callback(err, client);
     });
   },
   function (err, client) {
     if (err) {
-      console.error("Giving up");
+      console.error('Giving up');
       return;
     }
-    console.log("Connected to db");
+    console.log('Connected to db');
     getVotes(client);
   }
 );
 
 function getVotes(client) {
   client.query(
-    "SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote",
+    'SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote',
     [],
     function (err, result) {
       if (err) {
-        console.error("Error performing query: " + err);
+        console.error('Error performing query: ' + err);
       } else {
         var votes = collectVotesFromResult(result);
-        io.sockets.emit("scores", JSON.stringify(votes));
+        io.sockets.emit('scores', JSON.stringify(votes));
       }
 
       setTimeout(function () {
@@ -75,24 +75,24 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Origin', '*');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   next();
 });
 
-app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + '/views'));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.resolve(__dirname + "/views/index.html"));
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
 server.listen(port, function () {
   var port = server.address().port;
-  console.log("App running on port " + port);
+  console.log('App running on port ' + port);
 });
