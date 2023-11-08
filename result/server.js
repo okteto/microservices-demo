f1237586219c3cf74db1c67c1c0e5cce320e6d33
Ramon Lamana <rlamana@gmail.com>
2023-11-08 17:41:36 +0100

    Update pg library

--- result/server.js
+++ result/server.js
@@ -1,45 +1,43 @@
-var express = require('express'),
-    async = require('async'),
-    pg = require('pg'),
-    { Pool } = require('pg'),
-    path = require('path'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+var express = require("express"),
+  async = require('async'),
+  pg = require("pg"),
+  path = require("path"),
+  cookieParser = require("cookie-parser"),
+  methodOverride = require("method-override"),
+  app = express(),
+  server = require("http").Server(app),
+  io = require("socket.io")(server);
 
-io.set('transports', ['polling']);
+io.set("transports", ["polling"]);
 
 var port = process.env.PORT || 4000;
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
+  socket.emit("message", { text: "Welcome!" });
 
-  socket.emit('message', { text : 'Welcome!' });
-
-  socket.on('subscribe', function (data) {
+  socket.on("subscribe", function (data) {
     socket.join(data.channel);
   });
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://okteto:okteto@postgresql/votes'
+  connectionString: "postgres://okteto:okteto@postgresql/votes",
 });
 
 async.retry(
-  {times: 1000, interval: 1000},
-  function(callback) {
-    pool.connect(function(err, client, done) {
+  { times: 1000, interval: 1000 },
+  function (callback) {
+    pool.connect(function (err, client, done) {
       if (err) {
         console.error("Waiting for db", err);
       }
       callback(err, client);
     });
   },
-  function(err, client) {
+  function (err, client) {
     if (err) {
-      return console.error("Giving up");
+      console.error("Giving up");
+      return;
     }
     console.log("Connected to db");
     getVotes(client);
@@ -47,20 +45,26 @@ async.retry(
 );
 
 function getVotes(client) {
-  client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
-    if (err) {
-      console.error("Error performing query: " + err);
-    } else {
-      var votes = collectVotesFromResult(result);
-      io.sockets.emit("scores", JSON.stringify(votes));
-    }
+  client.query(
+    "SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote",
+    [],
+    function (err, result) {
+      if (err) {
+        console.error("Error performing query: " + err);
+      } else {
+        var votes = collectVotesFromResult(result);
+        io.sockets.emit("scores", JSON.stringify(votes));
+      }
 
-    setTimeout(function() {getVotes(client) }, 1000);
-  });
+      setTimeout(function () {
+        getVotes(client);
+      }, 1000);
+    }
+  );
 }
 
 function collectVotesFromResult(result) {
-  var votes = {a: 0, b: 0};
+  var votes = { a: 0, b: 0 };
 
   result.rows.forEach(function (row) {
     votes[row.vote] = parseInt(row.count);
@@ -71,21 +75,24 @@ function collectVotesFromResult(result) {
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(function(req, res, next) {
+app.use(methodOverride("X-HTTP-Method-Override"));
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + "/views"));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/views/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
 server.listen(port, function () {
   var port = server.address().port;
-  console.log('App running on port ' + port);
+  console.log("App running on port " + port);
 });
