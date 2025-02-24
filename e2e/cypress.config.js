7d096f703c2047de03b5aef322431de4994d173f
Ramiro <ramiro@okteto.com>
2025-02-24 15:34:08 -0800

    add e2e tests with cypress

---
+++ e2e/cypress.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'projectId': 'okteto',
+    e2e: {
+      voteUrl: 'https://vote-cindy.okteto.example.com',
+      resultUrl: 'https://result-cindy.okteto.example.com'
+    },
+  }
---
+++ e2e/cypress/e2e/check-if-online/spec.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+it('check that vote has the right title', () => {
+    cy.visit(Cypress.env('voteUrl'))
+    cy.title().should('eq', 'Burritos vs Tacos!')
+})
+
+it('check that result has the right title', () => {
+    cy.visit(Cypress.env('resultUrl'))
+    cy.title().should('eq', 'Burritos vs Tacos -- Result')
+})
+
+it('i love burritos', () => {
+    cy.visit(Cypress.env('voteUrl'))
+    cy.get('#a').click();
+
+    // Assert that the button is disabled
+    cy.get('#a').should('be.disabled');
+
+    // Assert that checkbox was displayed
+    cy.get('#a')
+      .find('i')
+      .should('exist')
+      .and('have.class', 'fa')
+      .and('have.class', 'fa-check-circle');
+})
